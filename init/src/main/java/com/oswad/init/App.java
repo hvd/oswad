0948dbceab18960c7eba3062e9b3bc82a3fd253b
Harshvardhan Kelkar <hkelkar@hkelkar-vaio.(none)>
2012-11-05 22:45:18 -0800

    first commit

---
+++ init/src/main/java/com/oswad/init/App.java
@@ -0,0 +1,13 @@
+package com.oswad.init;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
